c256d40773eab8b675b1dc0933d2982d39da9730
GiovanniGith <Gbarajas2109@gmail.com>
2022-07-19 20:47:08 -0500

    Add project files.

---
+++ PracticeRun2/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeRun2;
+
+internal class Person
+
+{
+    public Person(string name,
+                  string favoriteColor,
+                  string favoriteFood)
+    {
+        Name = name;
+        FavoriteColor = favoriteColor;
+        FavoriteFood = favoriteFood;
+        }
+
+public string Name { get; set; }
+public string FavoriteColor { get; set; }
+public string FavoriteFood { get; set; }
+
+internal void DisplayInfo()
+{
+    string info = $@"
+Name: {Name}
+
+Favorite Color: {FavoriteColor}
+
+Favorite Food: {FavoriteFood}
+";
+    Console.WriteLine(info);
+}
+};
---
+++ PracticeRun2/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PracticeRun2;
+
+List<Person> people = new List<Person>();
+while (true)
+{
+    //ask for name
+    Console.Write("Please enter your name(type 'quit' to quit): ");
+
+    String nameInput = Console.ReadLine();
+    if (nameInput.ToLower() == "quit")
+    {
+        break;
+    }
+
+
+
+    // ask for fave color
+    Console.WriteLine();
+    Console.WriteLine("What's your favorite color?");
+
+    string colorInput = Console.ReadLine();
+
+    //ask for fave food
+    Console.WriteLine();
+    Console.WriteLine("What's your favorite food?");
+
+    string foodInput = Console.ReadLine();
+
+    Person person = new Person(nameInput, colorInput, foodInput);
+    people.Add(person);
+}
+
+foreach (Person person in people)
+{
+    person.DisplayInfo();
+
+}
+
+// summarize all info entered so far
+
+
+
+
+
+
+Console.ReadLine();
